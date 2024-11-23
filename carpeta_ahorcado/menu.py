1580f8b8799cda39461cdb60fc6715f69fd74f1d
Markus2210 <almironmarcos.4@gmail.com>
2024-11-22 23:10:04 -0300

    logica del ahorcado.py terminada

--- carpeta_ahorcado/menu.py
+++ carpeta_ahorcado/menu.py
@@ -2,7 +2,8 @@ from Paquete_Estructura import *
 from Paquetes_Validaciones import *
 
 
-nombre_usuario = validar_cadena_usuario("Ingrese su nombre \nahorcado@python~$:", 'Error al ingresar su nombre. Intente nuevamente',)
+nombre_usuario = validar_cadena_usuario(
+    "Ingrese su nombre \nahorcado@python~$:", 'Error al ingresar su nombre. Intente nuevamente',)
 mensaje_menu = f"\n\n[1] Jugar \n[2] Puntajes \n[3] Salir \n\nUsted selecciona la opción: "
 simular_cargando()
 while True:
@@ -10,9 +11,9 @@ while True:
 
     match menu:
         case 1:
-            
-            print(imprimir_monigote(1))
+            pass
+            # print(imprimir_monigote(1))
         case 2:
-            pass    
+            pass
         case 3:
             break
