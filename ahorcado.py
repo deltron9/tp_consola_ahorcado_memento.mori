01e07a5f373db44c06df376b5a07de56eeaa1364
Deltron <cristian_collante@hotmail.com>
2024-11-23 23:18:43 -0300

    moficaciones de la lógica

--- ahorcado.py
+++ ahorcado.py
@@ -3,58 +3,6 @@ from carpeta_ahorcado.Paquetes_Validaciones import *
 import random
 import json
 
-def jugar():
-
-    msj_idioma = 'SELECCIONE UNA OPCION\n\n[es/en]\n\n\tOPCION: '
-    palabras = cargar_palabras()  # Las palabras cargadas del Json
-    palabras_seleccionadas = seleccionar_palabra(palabras)  #Las palabras del random.choice
-    eleccion_palabra = palabra_elegida(msj_idioma, palabras_seleccionadas)
-    palabra_oculta = p_oculta(eleccion_palabra)
-    letras_usadas = []
-    vidas = 6
-    etapa = 1
-
-    while vidas > 0 and '_' in palabra_oculta:
-        etapa += 1
-        print("\nPalabra: ", end=" ")
-        for letra in palabra_oculta:
-            # Imprime cada letra de la palabra oculta separada por un espacio
-            print(letra, end=" ")
-
-        print(imprimir_monigote(1))
-        letra_ingresada_por_usuario = validar_cadena_en_juego(
-            "Ingrese una letra: ")
-        if letra_ingresada_por_usuario in letras_usadas:
-            print(f"\n Esta letra ya fue utilizada!")
-
-        letras_usadas.append(letra_ingresada_por_usuario)
-
-        # elif letra_ingresada_por_usuario in idioma:
-        # El usuario recibe un punto por cada letra adivinada
-        # estructura_datos_usuario["Puntaje"] += 1
-        # Se agrega la letra al diccionario de letras adivinadas
-        # ADEMAS REEMPLAZA LETRA OCULTA
-        # letras_correctas += 1
-        if letra_ingresada_por_usuario in eleccion_palabra:
-            print("\n Letra acertada !!!!!")
-            puntos += 1
-            # Reemplaza el _ por la letra elegida
-            # for i in range(eleccion_palabra):
-            #     if i == letra_ingresada_por_usuario:
-            #         palabra_oculta[i] = letra_ingresada_por_usuario
-            for i, l in enumerate(eleccion_palabra):
-                if l == letra:
-                    palabra_oculta[i] = letra_ingresada_por_usuario
-        else:
-            print(f"Letra incorrecta, te quedan {vidas - 1} vidas")
-            vidas -= 1
-            print(imprimir_monigote(etapa))
-
-        if '_' not in palabra_oculta:
-            print(f"\nGANASTEEE!! La palabra era {eleccion_palabra}. Obtuviste {puntos} puntos")
-        else:
-            print(f"PERDISTE!! La palabra era {eleccion_palabra}")
-
         # gUARDAR pUNTAJE
         nombre_usuario = input("Ingrese su nombre: ")
         puntajes = cargar_puntaje()
