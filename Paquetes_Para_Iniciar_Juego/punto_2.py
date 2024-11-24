1cbf7170c95cee808aee7e722d9a2a735596570e
Deltron <cristian_collante@hotmail.com>
2024-11-24 13:07:43 -0300

    modificaciones en el código

--- Paquetes_Para_Iniciar_Juego/punto_2.py
+++ Paquetes_Para_Iniciar_Juego/punto_2.py
@@ -1,7 +1,15 @@
 import json
 
 def mostrar_puntajes():
-    file = open("scores.json", "r+")
-    text = file.read()  # todo el contedido del archivo
-    print(text)
-    file.close()
+    with open("ahorcado/scores.json", "r") as file:
+        datos = json.load(file)
+
+    for usuario in datos:     # Recorrer la lista y mostrar los nombres y puntajes de los usuarios
+        print(f"Nombre: {usuario['nombre']} - Puntaje: {usuario['Puntaje']}")
+    '''
+    Loque hace el For es imprimir los datos de scores sin el formato de diccionario
+    si lo poniamos solamente con el return de datos, imprime por pantalla el json en crudo.
+    '''
+
+
+
