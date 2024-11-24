ecc2c27b51d2034c44bc456c8cf6cf15a319fd13
Deltron <cristian_collante@hotmail.com>
2024-11-24 17:20:58 -0300

    retoques a la función de bubble sort

--- Paquetes_Para_Iniciar_Juego/punto_2.py
+++ Paquetes_Para_Iniciar_Juego/punto_2.py
@@ -1,15 +1,22 @@
 import json
 
 def mostrar_puntajes():
+    """
+Esta función abre el archivo de scores en modo lectura y aloja los datos en la variable lista
+usando bubble sort ordenamos los 5 mejores punajes de manera ascendente
+y por último al llamar a la función, esta usa un for iterando  devuelve un print  
+    """
     with open("scores.json", "r") as file:
-        datos = json.load(file)
-
-    for usuario in datos:     # Recorrer la lista y mostrar los nombres y puntajes de los usuarios
-        print(f"Nombre: {usuario['nombre']} - Puntaje: {usuario['Puntaje']}")
-    '''
-    Loque hace el For es imprimir los datos de scores sin el formato de diccionario
-    si lo poniamos solamente con el return de datos, imprime por pantalla el json en crudo.
-    '''
-
-
-
+        lista = json.load(file)
+    
+    for i in range(len(lista)):                   #itera la lista
+        for it in range(0, len(lista)-i-1): #itera cada elementos de la lista (desde indice 0 y muere 1 indice antes de i)
+            if lista[it]['Puntaje'] < lista[it + 1]['Puntaje']:
+                red = lista[it]                   #Guardamos el elemento actual
+                lista[it] = lista[it + 1]         #Movemos el elemento siguiente a la posición actual
+                lista[it + 1] = red               #Colocamos el elemento guardado en la siguiente posición
+    
+    #Mostrar solo los 5 mejores puntajes
+    print("\n\tTOP 5 MEJORES PUNTAJES MUNDIALES")
+    for jugador in lista[:5]:
+        print(f"\n\t{jugador['nombre']}: {jugador['Puntaje']} puntos")
