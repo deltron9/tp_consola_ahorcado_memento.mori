6afc2da98f0d64b6656db3bbeaf8411d9e103d79
Deltron <cristian_collante@hotmail.com>
2024-11-24 16:54:27 -0300

    cambios: impresionde texto en la sección  de simulación de carga

--- Paquetes_Interfaz_De_Consola/simulacion_de_carga.py
+++ Paquetes_Interfaz_De_Consola/simulacion_de_carga.py
@@ -33,12 +33,12 @@ def simular_cargando(mensaje: str = 'Cargando') -> str:
 #Esta función simula un carga de juego y muestra el mensaje en pantalla de la variable BIENVENIDA
     mensaje_impresion = mensaje
     suma_puntos = 0
-    print(f"{mensaje_impresion}")
+    print(f"\t\t\t\t{mensaje_impresion}")
     for i in range(3):
         mensaje_impresion += '.'
-        print(f"{mensaje_impresion}")
+        print(f"\t\t\t\t{mensaje_impresion}")
         time.sleep(1)  #Pausa de un segundo entre cada impresión
         suma_puntos += 1
         if suma_puntos == 3:
-            print(f'¡Carga completa!')
+            print(f'\t\t\t\t¡Carga completa!\n')
             time.sleep(2)
